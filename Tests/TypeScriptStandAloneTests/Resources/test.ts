d7dc193749484aed320a20cc4bb3eb13c14b59ac
Oliver Atkinson <oliveratkinson@me.com>
2022-08-12 21:39:49 +0100

    Update test.ts

--- Tests/TypeScriptStandAloneTests/Resources/test.ts
+++ Tests/TypeScriptStandAloneTests/Resources/test.ts
@@ -1,23 +1,16 @@
-// I
-interface I extends TypeLocalized, SourceCodeIdentifiable { }
-interface TypeLocalized {
-  localized: string;
-}
-interface SourceCodeIdentifiable {
-  __: string;
-  debugDescription: string;
-}
+interface I { }
+
 // L
 class L implements I {
-  localized: string;
-  __: string;
-  get debugDescription() { return this.__ }
-
-  constructor(id: string, localized = "") {
-    this.localized = localized;
-    this.__ = id;
-  }
+	protected id: string;
+	constructor(id: string) {
+		this.id = id;
+	}
+	get ['__']() {
+		return this.id;
+	}
 }
+
 // MARK: generated types
 class L_test extends L implements I_test {
   one = new L_test_one(`${this.__}.one`);
@@ -25,22 +18,22 @@ class L_test extends L implements I_test {
   type = new L_test_type(`${this.__}.type`);
 }
 interface I_test extends I {
-  one: L_test_one;
-  two: L_test_two;
-  type: L_test_type;
+  one: I_test_one;
+  two: I_test_two;
+  type: I_test_type;
 }
 class L_test_one extends L implements I_test_one {
   good!: L_test_type_odd_good;
   more = new L_test_one_more(`${this.__}.more`);
 }
 interface I_test_one extends I_test_type_odd {
-  more: L_test_one_more;
+  more: I_test_one_more;
 }
 class L_test_one_more extends L implements I_test_one_more {
   time = new L_test_one_more_time(`${this.__}.time`);
 }
 interface I_test_one_more extends I {
-  time: L_test_one_more_time;
+  time: I_test_one_more_time;
 }
 class L_test_one_more_time extends L implements I_test_one_more_time {
   one!: L_test_one;
@@ -54,7 +47,7 @@ class L_test_two extends L implements I_test_two {
   timing = new L_test_two_timing(`${this.__}.timing`);
 }
 interface I_test_two extends I_test_type_even {
-  timing: L_test_two_timing;
+  timing: I_test_two_timing;
 }
 class L_test_two_timing extends L implements I_test_two_timing {
 }
@@ -64,22 +57,22 @@ class L_test_type extends L implements I_test_type {
   odd = new L_test_type_odd(`${this.__}.odd`);
 }
 interface I_test_type extends I {
-  even: L_test_type_even;
-  odd: L_test_type_odd;
+  even: I_test_type_even;
+  odd: I_test_type_odd;
 }
 class L_test_type_even extends L implements I_test_type_even {
   no = new L_test_type_even_no(`${this.__}.no`);
   bad = this.no.good;
 }
 interface I_test_type_even extends I {
-  no: L_test_type_even_no;
+  no: I_test_type_even_no;
 }
 type L_test_type_even_bad = L_test_type_even_no_good
 class L_test_type_even_no extends L implements I_test_type_even_no {
   good = new L_test_type_even_no_good(`${this.__}.good`);
 }
 interface I_test_type_even_no extends I {
-  good: L_test_type_even_no_good;
+  good: I_test_type_even_no_good;
 }
 class L_test_type_even_no_good extends L implements I_test_type_even_no_good {
 }
@@ -88,7 +81,7 @@ class L_test_type_odd extends L implements I_test_type_odd {
   good = new L_test_type_odd_good(`${this.__}.good`);
 }
 interface I_test_type_odd extends I {
-  good: L_test_type_odd_good;
+  good: I_test_type_odd_good;
 }
 class L_test_type_odd_good extends L implements I_test_type_odd_good {
 }
