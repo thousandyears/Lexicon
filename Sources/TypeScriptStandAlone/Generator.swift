f5345e5ded16cf5c269fc004a8d0c8645f3d71c1
Milos <milos@screensailor.net>
2024-03-20 18:03:24 +0000

    remove the previous implementation

--- Sources/TypeScriptStandAlone/Generator.swift
+++ Sources/TypeScriptStandAlone/Generator.swift
@@ -12,8 +12,6 @@ public extension UTType {
 
 public enum Generator: CodeGenerator {
 	
-	// TODO: prefixes?
-	
 	public static let utType = UTType.typescript
 	public static let command = "ts"
 
@@ -127,103 +125,6 @@ private extension Lexicon.Graph.Node.Class.JSON {
 			o.merge(type.allChildren(in: classes), uniquingKeysWith: {_, last in last})
 		}
 		return o
-	}
-	
-	// TODO: make this more readable
-	
-	func ts2(prefix: (class: String, protocol: String), classes: [Lexicon.Graph.Node.Class.JSON]) -> [String] {
-		
-		guard mixin == nil else {
-			return []
-		}
-		
-		var lines: [String] = []
-		let T = id.idToClassSuffix
-		let (L, I) = prefix
-		
-		if let protonym = protonym {
-			lines += "type \(L)_\(T) = \(L)_\(protonym.idToClassSuffix)"
-			return lines
-		}
-		
-		lines += "class \(L)_\(T) extends \(L) implements \(I)_\(T) {"
-
-		let supertype = supertype?
-			.replacingOccurrences(of: "_", with: "__")
-			.replacingOccurrences(of: ".", with: "_")
-			.replacingOccurrences(of: "__&__", with: ", I_")
-		
-		if hasNoProperties {
-			if supertype != nil {
-				let superChildren = classes.filter {$0.id == supertype}.first?.children
-
-				for child in superChildren ?? [] {
-					lines += "  \(child)!: \(L)_\(supertype!)_\(child);"
-				}
-				
-				for t in type ?? [] {
-					let subClass = classes.filter{$0.id == t}.first
-					for child in subClass?.children ?? [] {
-						let id = "L.\(t).\(child)"
-						lines += "  get \(child)() { return new \(id.idToClassSuffix)(`${this._id}.\(child)`) }"
-					}
-					if let keys = subClass?.synonyms?.keys {
-						for synonym in keys {
-							let id = "L.\(t).\(synonym)"
-							lines += "  \(synonym)!: \(id.idToClassSuffix);"
-						}
-					}
-					
-				}
-				
-				lines += "}"
-
-				lines += "interface \(I)_\(T) extends I_\(supertype!) {}"
-			} else {
-				lines += "}"
-				lines += "interface \(I)_\(T) extends I {}"
-			}
-		}
-		
-		guard hasProperties else {
-			lines += ""
-			return lines
-		}
-		
-		for t in type ?? [] {
-			let subClass = classes.filter{$0.id == t}.first
-			for child in subClass?.children ?? [] {
-				let id = "L.\(t).\(child)"
-				lines += "  \(child)!: \(id.idToClassSuffix);"
-			}
-			if let keys = subClass?.synonyms?.keys {
-				for synonym in keys {
-					let id = "L.\(t).\(synonym)"
-					lines += "  \(synonym)!: \(id.idToClassSuffix);"
-				}
-			}
-			
-		}
-		
-		for child in children ?? [] {
-			let id = "\(id).\(child)"
-			lines += "  get \(child)() { return new \(L)_\(id.idToClassSuffix)(`${this._id}.\(child)`) }"
-		}
-		
-		for (synonym, protonym) in (synonyms?.sortedByLocalizedStandard(by: \.key) ?? []) {
-			lines += "  \(synonym) = this.\(protonym);"
-		}
-		lines += "}"
-		
-		lines += "interface \(I)_\(T) extends \(I)\(supertype.map{ "_\($0)" } ?? "") {"
-		
-		for child in children ?? [] {
-			let id = "\(id).\(child)"
-			lines += "  \(child): \(I)_\(id.idToClassSuffix);"
-		}
-		lines += "}"
-		lines += ""
-		return lines
 	}
 }
 
