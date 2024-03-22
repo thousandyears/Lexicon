9fc1e0945ba48a4ed519d09ea1c87a5743a1e3d0
Milos <milos@screensailor.net>
2024-03-22 14:04:33 +0000

    refactor

--- Sources/mind/MindDictionary.swift
+++ Sources/mind/MindDictionary.swift
@@ -21,9 +21,6 @@ struct MindDictionary: AsyncParsableCommand {
     
     @Option(name: .shortAndLong, help: "Type(s) to return")
     var type: [String]
-    
-    @Flag(name: .shortAndLong)
-    var quiet: Bool = false
 
     @Option(
         name: .shortAndLong,
@@ -31,51 +28,43 @@ struct MindDictionary: AsyncParsableCommand {
     )
     var output: URL?
 
-    private var isLogging: Bool { !quiet }
-
     @LexiconActor
     mutating func run() async throws {
-        if isLogging {
-            print("Reading \(lexicon.lastPathComponent)...")
-        }
         let lexicon = try Lexicon.from(
             TaskPaper(Data(contentsOf: self.lexicon)).decode()
         )
-        var result: [String: Any] = [:]
-        let type = Set(type)
-        
-        for (id, lemma) in lexicon.dictionary where !type.contains(id) {
-            let lemmaType = Set(lemma.type.map(\.1.id))
-            guard type.isSubset(of: lemmaType) else {
-                continue
-            }
-            result[id] = lemma.name
-        }
         
         let file = output ?? self.lexicon.deletingLastPathComponent()
                 .appendingPathComponent(lexicon.root.name)
                 .appendingPathExtension("json")
         
-        if var loaded = try? JSONSerialization.jsonObject(with: Data(contentsOf: file)) as? [String: Any] {
-            for id in Set(loaded.keys).subtracting(result.keys) {
-                loaded.removeValue(forKey: id)
-            }
-            result = loaded.merging(result, uniquingKeysWith: { loaded, _ in loaded })
-        }
+        let loaded = try? JSONSerialization.jsonObject(with: Data(contentsOf: file)) as? [String: Any]
+        
+        let result = dictionary(lexicon: lexicon, type: Set(type), loaded: loaded ?? [:])
         
         try JSONSerialization.data(withJSONObject: result, options: [.prettyPrinted, .sortedKeys])
             .write(to: file)
     }
-}
 
-typealias Generators = OrderedDictionary<String, CodeGenerator.Type>
+    @LexiconActor
+    private func dictionary(
+        lexicon: Lexicon,
+        type: Set<String>,
+        loaded: [String: Any]
+    ) -> [String: Any] {
+        var result: [String: Any] = [:]
+        
+        lexicon.root.graphTraversal(.depthFirst) { lemma in
+            guard type.isSubset(of: Set(lemma.type.map(\.1.id))) else {
+                return
+            }
+            result[lemma.id] = lemma.name
+        }
+        
+        result = loaded
+            .filter{ id, _ in Set(result.keys).contains(id) }
+            .merging(result, uniquingKeysWith: { loaded, _ in loaded })
 
-extension Generators {
-
-    var commandHelp: String { values.map { $0.command }.joined(separator: ", ") }
-
-    func find(_ command: String) -> CodeGenerator.Type? {
-        first { _, value in value.command == command }?.value
+        return result
     }
 }
-
